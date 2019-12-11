6790365400c31897ba3394607be7008ebaad544c
Unknown <tcphone93@gmail.com>
2019-12-11 04:14:28 +0000

    push day 10 lol

---
+++ day10.py
@@ -0,0 +1,135 @@
+from math import gcd, atan2, pi
+
+map = """.#......##.#..#.......#####...#..
+...#.....##......###....#.##.....
+..#...#....#....#............###.
+.....#......#.##......#.#..###.#.
+#.#..........##.#.#...#.##.#.#.#.
+..#.##.#...#.......#..##.......##
+..#....#.....#..##.#..####.#.....
+#.............#..#.........#.#...
+........#.##..#..#..#.#.....#.#..
+.........#...#..##......###.....#
+##.#.###..#..#.#.....#.........#.
+.#.###.##..##......#####..#..##..
+.........#.......#.#......#......
+..#...#...#...#.#....###.#.......
+#..#.#....#...#.......#..#.#.##..
+#.....##...#.###..#..#......#..##
+...........#...#......#..#....#..
+#.#.#......#....#..#.....##....##
+..###...#.#.##..#...#.....#...#.#
+.......#..##.#..#.............##.
+..###........##.#................
+###.#..#...#......###.#........#.
+.......#....#.#.#..#..#....#..#..
+.#...#..#...#......#....#.#..#...
+#.#.........#.....#....#.#.#.....
+.#....#......##.##....#........#.
+....#..#..#...#..##.#.#......#.#.
+..###.##.#.....#....#.#......#...
+#.##...#............#..#.....#..#
+.#....##....##...#......#........
+...#...##...#.......#....##.#....
+.#....#.#...#.#...##....#..##.#.#
+.#.#....##.......#.....##.##.#.##""".splitlines()
+# map = """.#..##.###...#######
+# ##.############..##.
+# .#.######.########.#
+# .###.#######.####.#.
+# #####.##.#.##.###.##
+# ..#####..#.#########
+# ####################
+# #.####....###.#.#.##
+# ##.#################
+# #####.##.###..####..
+# ..######..##.#######
+# ####.##.####...##..#
+# .#####..#.######.###
+# ##...#.##########...
+# #.##########.#######
+# .####.#.###.###.#.##
+# ....##.##.###..#####
+# .#.#.###########.###
+# #.#.#.#####.####.###
+# ###.##.####.##.#..##""".splitlines()
+# target = 5, 8
+width = height = len(map)
+
+
+def get_line_of_sight(x, y):
+    points = []
+    for nx in range(x - 1, -1, -1):
+        if map[y][nx] == "#":
+            points.append((nx, y))
+            break
+    for nx in range(x + 1, width):
+        if map[y][nx] == "#":
+            points.append((nx, y))
+            break
+    for ny in range(y - 1, -1, -1):
+        if map[ny][x] == "#":
+            points.append((x, ny))
+            break
+    for ny in range(y + 1, width):
+        if map[ny][x] == "#":
+            points.append((x, ny))
+            break
+
+    for ox in range(-1, -width, -1):
+        for oy in range(-1, -width, -1):
+            if gcd(oy, ox) != 1:
+                continue
+            for oymult in [-1, 1]:
+                for multiplier in range(1, width):
+                    nx = x + ox * multiplier
+                    ny = y + oy * multiplier * oymult
+                    # print(nx, ny, ox, oy, oymult, multiplier)
+                    if 0 <= nx < width and 0 <= ny < height:
+                        if map[ny][nx] == "#":
+                            points.append((nx, ny))
+                            break
+    for ox in range(1, width):
+        for oy in range(1, width):
+            if gcd(oy, ox) != 1:
+                continue
+            for oymult in [-1, 1]:
+                for multiplier in range(1, width):
+                    nx = x + ox * multiplier
+                    ny = y + oy * multiplier * oymult
+                    # print(nx, ny, ox, oy, oymult, multiplier)
+                    if 0 <= nx < width and 0 <= ny < height:
+                        if map[ny][nx] == "#":
+                            points.append((nx, ny))
+                            break
+
+    # for index_y, row in enumerate(map):
+    #     for index_x, tile in enumerate(row):
+    #         if (index_x, index_y) == (x, y):
+    #             print(f"[{tile}]", end="")
+    #         elif (index_x, index_y) in points:
+    #             print(f"({tile})", end="")
+    #         else:
+    #             print(f" {tile} ", end="")
+    #     print()
+    # print()
+    return set(points)
+
+
+tiles = {}
+for y, row in enumerate(map):
+    for x, tile in enumerate(row):
+        if tile == "#":
+            tiles[len(get_line_of_sight(x, y))] = x, y
+key = max(tiles.keys())
+print(key, tiles[key])
+x, y = tiles[key]
+asteroids = get_line_of_sight(x, y)
+sorted_points = sorted(
+    asteroids, key=lambda point: atan2(point[0] - x, y - point[1]) % (2 * pi)
+)
+point = sorted_points[199]
+# print(sorted_points.index((8, 2)))
+# print(sorted_points[100:115])
+print(point[0] * 100 + point[1])
+# get_line_of_sight(*target)
